a7a30232d03c7acd2cc171455f7380cdce988ab1
Intern-Yago <yagovictorbotafogo@gmail.com>
2022-12-30 23:29:55 -0300

    express static

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 require('dotenv').config()
 
-app.use(express.static("public"))
+app.use(express.static(__dirname + '/public'));
 
 app.get('/', (req, res) => {
         let name = req.query["name"]
